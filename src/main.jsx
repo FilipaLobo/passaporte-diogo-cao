1c5150cc9c3c7c826ef5ec82ed45da468b90b920
FilipaLobo <filipalobo@diogocao.edu.pt>
2025-05-30 18:20:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PassaporteDigitalApp from "./PassaporteDigitalApp";
+import App from "./PassaporteDigitalApp";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
